aef952237f1dd8a0ccb4c1ae2304b4a30d7565c1
Germano Gallicchio <germano.gallicchio@gmail.com>
2025-08-28 01:23:27 +0100

    Update po_recenter.m

--- po_recenter.m
+++ po_recenter.m
@@ -1,11 +1,12 @@
-function [theta_centered, rho_centered ] = po_recenter(theta, rho)
+function [theta_centered, rho_centered ] = po_demean(theta, rho)
 
 % DESCRIPTION: Center to the phase-only mean along first dimension
 %
 % INPUT:        
 %
 %
-%
+% usage: feed all 1s as rho for dPAC, then element wise product of magnitude and complex vector created by combining rho_centered and theta_centered
+% 
 % AUTHOR:
 %   Germano Gallicchio (germano.gallicchio@gmail.com)
 
@@ -14,8 +15,10 @@ function [theta_centered, rho_centered ] = po_recenter(theta, rho)
 
 %% implementation
 
-theta_mean = mean(exp(1i*theta),1);  % phase clustering over dimension 1
-centered_complex = exp(1i*theta) - theta_mean;   % complex difference vectors
+compl_mean = mean(rho.*exp(1i*theta),1); 
+
+% phase clustering over dimension 1
+centered_complex = rho.*exp(1i*theta) - compl_mean;   % complex difference vectors
 
 
 theta_centered = angle(centered_complex);
