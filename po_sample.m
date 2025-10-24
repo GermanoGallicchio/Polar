4ec8f84e9b25fc58ec517d9ce3c94fce98ab2c4e
Germano Gallicchio <germano.gallicchio@gmail.com>
2025-10-24 12:14:17 +0100

    Update po_sample.m

--- po_sample.m
+++ po_sample.m
@@ -1,32 +1,67 @@
-function [theta, rho] = po_simulate(simulationParams)
+function [theta, r] = po_sample(po_cfg)
 
 % DESCRIPTION:
 %
 % INPUT:        
+%   po_cfg
 %
-% OPTIONAL:
 %
+%
+% OPTIONAL INPUT:
+%   none
+%
+%
+% OUTPUT:
+%   theta  -    vector angles (in radians)
+%   r      -    vector lengths 
 %
 % AUTHOR:
 %   Germano Gallicchio (germano.gallicchio@gmail.com)
 
+%% playground section (try some parameters)
+
+toggle = true;
+
+if toggle
+
+po_cfg.sampling.distribution = 'vonMises';  % 'vonMises' | 'wrappedCauchy'
+po_cfg.sampling.theta_mu = 
+po_cfg.sampling.theta_kappa
+
+end
+
+%% input check and defaults
+
+% sanity check: vonMises distribution requires parameters mu and kappa
+% if simulating theta, we need mu and kappa for theta
+% if simulating r, we need mu and kappa for r
+% if simulating both, we need mu and kappa for each
+% TO DO
+
+% sanity check: wrappedCauchy distribution requires parameters mu and rho
+% if simulating theta, we need mu and kappa for theta
+% if simulating r, we need mu and kappa for r
+% if simulating both, we need mu and kappa for each
+% TO DO
+
+
 %% shortcuts
 
-nSamples  = simulationParams.nSamples;
-kappa     = simulationParams.kappa;
-mu        = simulationParams.mu;
-theta_rho = simulationParams.theta_rho;
+nSamples  = po_cfg.nSamples;
+kappa     = po_cfg.kappa;
+mu        = po_cfg.mu;
+theta_rho = po_cfg.theta_rho;
 nModes = length(mu);
 
 %% random seed
 
 
-fieldLbl = fieldnames(simulationParams);
+fieldLbl = fieldnames(po_cfg);
 if ~any(strcmp(fieldLbl,'randomSeed'))
     randomSeed = 42;
     warning(['setting random seed for you: ' num2str(randomSeed)])
 else
-    randomSeed = simulationParams.randomSeed;
+    randomSeed = po_cfg.randomSeed;
 end
 
 rng(randomSeed)
@@ -36,21 +71,21 @@ switch num2str(theta_rho)
         % build an empirical density distribution by drawing from a
         % theoretical distribution (sum of von Mises).
     
-        fieldLbl = fieldnames(simulationParams);
+        fieldLbl = fieldnames(po_cfg);
         if ~any(strcmp(fieldLbl,'theta'))
             thetaLim = [-pi, pi];  % create a range of phase values in (-pi, pi]
             theta = linspace(thetaLim(1), thetaLim(2), nSamples+1);
             theta(1) = [];
         else
-            theta = simulationParams.theta;
+            theta = po_cfg.theta;
         end
 
         % empirical density distribution 
-        rho = po_vonMisesDensity(kappa,mu,theta);
+        r = po_vonMisesDensity(kappa,mu,theta);
 
         % ensure vectors are column
         theta = theta(:);
-        rho = rho(:);
+        r = r(:);
 
     case num2str([1 0])  % simulate theta, with unit rho
         % drawing theta samples (step 3) from a probability mass
@@ -62,9 +97,9 @@ switch num2str(theta_rho)
         thetaLim = [-pi, pi];  % create a range of phase values in (-pi, pi]
         theta = linspace(thetaLim(1), thetaLim(2), 10000+1);
         theta(1) = [];
-        rho = po_vonMisesDensity(kappa,mu,theta);
+        r = po_vonMisesDensity(kappa,mu,theta);
         % step 2. probability mass distribution
-        pmf = rho * (theta(2) - theta(1)); % approximate area per bin
+        pmf = r * (theta(2) - theta(1)); % approximate area per bin
         pmf = pmf / sum(pmf);              % make sure it sums to 1
         % step 3. draw theta samples from probability mass distribution
         sampleIdx = randsample(length(theta), nSamples, true, pmf);
@@ -72,12 +107,12 @@ switch num2str(theta_rho)
 
         % ensure vectors are column
         theta = samples(:);
-        rho = ones(size(theta));
+        r = ones(size(theta));
 
     
 
     otherwise  % simulate both theta and rho
-        if strcmp(num2str(simulationParams.theta_rho),num2str([1 1]))  % simulate both theta and rho
+        if strcmp(num2str(po_cfg.theta_rho),num2str([1 1]))  % simulate both theta and rho
             warning(['if you want to simulate both theta and rho at once,' ...
                 ' do it in two steps' ...
                 ' 1) simulate theta with its parameters' ...
