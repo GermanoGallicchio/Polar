42ae0053b77a0de380c7732b7b47a42186858c8d
Germano Gallicchio <germano.gallicchio@gmail.com>
2025-11-04 17:05:47 +0000

    Update po_sample.m

--- po_sample.m
+++ po_sample.m
@@ -24,48 +24,258 @@ toggle = true;
 
 if toggle
 
-po_cfg.sampling.distribution = 'vonMises';  % 'vonMises' | 'wrappedCauchy'
-po_cfg.sampling.theta_mu = 
-po_cfg.sampling.theta_kappa
+% decide sampling parameters
+po_cfg.sampling.nSamples = 20;
+po_cfg.sampling.distribution.family = 'vonMises';  % 'vonMises' | 'wrappedCauchy'
+po_cfg.sampling.theta = true;   % choose if sampling theta
+po_cfg.sampling.r     = false;  % choose if sampling r
+po_cfg.sampling.distribution.theta.kappa  = 1; % concentration parameter for von Mises
+%po_cfg.sampling.distribution.theta.rho    = 0.5; % concentration parameter for wrapped Cauchy
+po_cfg.sampling.distribution.theta.mu     = pi; % direction parameter for either von Mises or wrapped Cauchy
+%po_cfg.sampling.distribution.r.kappa  = 1; % concentration parameter for von Mises
+%po_cfg.sampling.distribution.r.rho    = 0.5; % concentration parameter for wrapped Cauchy
+%po_cfg.sampling.distribution.r.mu     = pi; % direction parameter for either von Mises or wrapped Cauchy
+
 
 end
 
-%% input check and defaults
+%% defaults
 
-% sanity check: vonMises distribution requires parameters mu and kappa
-% if simulating theta, we need mu and kappa for theta
-% if simulating r, we need mu and kappa for r
-% if simulating both, we need mu and kappa for each
-% TO DO
-
-% sanity check: wrappedCauchy distribution requires parameters mu and rho
-% if simulating theta, we need mu and kappa for theta
-% if simulating r, we need mu and kappa for r
-% if simulating both, we need mu and kappa for each
-% TO DO
-
-
-%% shortcuts
-
-nSamples  = po_cfg.nSamples;
-kappa     = po_cfg.kappa;
-mu        = po_cfg.mu;
-theta_rho = po_cfg.theta_rho;
-nModes = length(mu);
-
-%% random seed
-
-
+% verbose = true
 fieldLbl = fieldnames(po_cfg);
-if ~any(strcmp(fieldLbl,'randomSeed'))
-    randomSeed = 42;
-    warning(['setting random seed for you: ' num2str(randomSeed)])
-else
-    randomSeed = po_cfg.randomSeed;
+if any(strcmp(fieldLbl,'verbose')) ~= 1
+    po_cfg.verbose = true;
+    warning('po_cfg.verbose not defined by user. i am setting it to true, but you can specify it explicitly to avoid this warning')
 end
 
-rng(randomSeed)
+% randomSeed
+fieldLbl = fieldnames(po_cfg);
+if any(strcmp(fieldLbl,'randomSeed')) ~= 1
+    po_cfg.sampling.randomSeed = 42;
+    warning(['po_cfg.sampling.randomSeed not defined by user. i am setting it to ' num2str(randomSeed) ' , but you can specify it explicitly to avoid this warning'])
+end
+
+    
+%% input checks
+
+% sanity check: a "sampling" field exists
+fieldLbl = fieldnames(po_cfg);
+if any(strcmp(fieldLbl,'sampling')) ~= 1
+    error('po_cfg structure needs to have a "sampling" field')
+end
+
+% sanity check: sampling "distribution" field exists
+fieldLbl = fieldnames(po_cfg.sampling);
+if any(strcmp(fieldLbl,'distribution')) ~= 1
+    error('po_cfg.sampling structure needs to have a "distribution" field')
+end
+
+% sanity check: sampling distribution "family" field exists
+fieldLbl = fieldnames(po_cfg.sampling.distribution);
+if any(strcmp(fieldLbl,'family')) ~= 1
+    error('po_cfg.sampling.distribution structure needs to have a "family" field')
+end
+
+% sanity check: sampling distribution family is one of the allowed ones
+familyDistributions = ["vonMises" "wrappedCauchy"];
+if ~ismember(po_cfg.sampling.distribution.family, familyDistributions)
+    disp(familyDistributions)
+    error('po_cfg.sampling.distribution.family needs to be one of the above')
+end
+
+% sanity check: user has decided if wanting to sample theta
+fieldLbl = fieldnames(po_cfg.sampling);
+if any(strcmp(fieldLbl,'theta')) ~= 1
+    error('po_cfg.sampling structure needs to have a "theta" field')
+end
+% sanity check: theta needs to be a logical value
+if ~islogical(po_cfg.sampling.theta)
+    error('theta needs to be a logical value (true = you want to sample it')
+end
+
+% sanity check: user has decided if wanting to sample r
+fieldLbl = fieldnames(po_cfg.sampling);
+if any(strcmp(fieldLbl,'r')) ~= 1
+    error('po_cfg.sampling structure needs to have a "r" field')
+end
+% sanity check: r needs to be a logical value
+if ~islogical(po_cfg.sampling.r)
+    error('theta needs to be a logical value (true = you want to sample it')
+end
+
+% sanity check: if both po_cfg.sampling.theta and po_cfg.sampling.theta are false, there is nothing to sample
+if po_cfg.sampling.theta==false  &&  po_cfg.sampling.r==false
+    error('nothing to sample: both po_cfg.sampling.theta and po_cfg.sampling.r are set to false')
+end
+
+% sanity check: check the right parameters are given for the distribution
+switch po_cfg.sampling.distribution.family
+    case 'vonMises' % vonMises distribution requires parameters kappa and mu
+
+        if po_cfg.sampling.theta
+            fieldLbl = fieldnames(po_cfg.sampling.distribution.theta);
+            if any(strcmp(fieldLbl,'kappa')) ~= 1
+                error('po_cfg.sampling.distribution.theta.kappa is required')
+            end
+            if any(strcmp(fieldLbl,'mu')) ~= 1
+                error('po_cfg.sampling.distribution.theta.mu is required')
+            end
+        end
+
+        if po_cfg.sampling.r
+            fieldLbl = fieldnames(po_cfg.sampling.distribution.r);
+            if any(strcmp(fieldLbl,'kappa')) ~= 1
+                error('po_cfg.sampling.distribution.r.kappa is required')
+            end
+            if any(strcmp(fieldLbl,'mu')) ~= 1
+                error('po_cfg.sampling.distribution.r.mu is required')
+            end
+        end
+
+    case 'wrappedCauchy' % wrappedCauchy distribution requires parameters rho and mu
+
+        if po_cfg.sampling.theta
+            fieldLbl = fieldnames(po_cfg.sampling.distribution.theta);
+            if any(strcmp(fieldLbl,'rho')) ~= 1
+                error('po_cfg.sampling.distribution.theta.rho is required')
+            end
+            if any(strcmp(fieldLbl,'mu')) ~= 1
+                error('po_cfg.sampling.distribution.theta.mu is required')
+            end
+        end
+
+        if po_cfg.sampling.r
+            fieldLbl = fieldnames(po_cfg.sampling.distribution.r);
+            if any(strcmp(fieldLbl,'rho')) ~= 1
+                error('po_cfg.sampling.distribution.r.rho is required')
+            end
+            if any(strcmp(fieldLbl,'mu')) ~= 1
+                error('po_cfg.sampling.distribution.r.mu is required')
+            end
+        end
+
+end
+
+
+% sanity check: specified how many samples to draw from this distribution
+fieldLbl = fieldnames(po_cfg.sampling);
+if any(strcmp(fieldLbl,'nSamples')) ~= 1
+    error('po_cfg.sampling structure needs to have a "nSample" field')
+end
+% sanity check: nSamples is a number
+if ~isnumeric(po_cfg.sampling.nSamples)
+    error('po_cfg.sampling.nSamples must be numeric')
+end
+
+
+
+%% shortcuts 
+
+% for distribution parameters
+nSamples    = po_cfg.sampling.nSamples;
+familyDistr = po_cfg.sampling.distribution.family;
+randomSeed = po_cfg.randomSeed;
+switch familyDistr
+    case 'vonMises'
+
+        if po_cfg.sampling.theta
+            theta_concentration = po_cfg.sampling.distribution.theta.kappa;
+            theta_direction = po_cfg.sampling.distribution.theta.mu;
+        else
+            theta_concentration = 'none';
+            theta_direction = 'none';
+        end
+
+        if po_cfg.sampling.r
+            r_concentration = po_cfg.sampling.distribution.r.kappa;
+            r_direction = po_cfg.sampling.distribution.r.mu;
+        else
+            r_concentration = 'none';
+            r_direction = 'none';
+        end
+
+    case 'wrappedCauchy'
+
+        if po_cfg.sampling.theta
+            theta_concentration = po_cfg.sampling.distribution.theta.rho;
+            theta_direction = po_cfg.sampling.distribution.theta.mu;
+        else
+            theta_concentration = 'none';
+            theta_direction = 'none';
+        end
+        
+        if po_cfg.sampling.r
+            r_concentration = po_cfg.sampling.distribution.r.rho;
+            r_direction = po_cfg.sampling.distribution.r.mu;
+        else
+            r_concentration = 'none';
+            r_direction = 'none';
+        end
+end
+
+%% feedback to user on what they want to sample
+if po_cfg.verbose
+    fprintf('\n')
+    if po_cfg.sampling.theta  &&  ~po_cfg.sampling.r % only theta
+        textLbl = "you want to sample %u values of theta from a %s distribution with parameters %f (concentration) and %f (direction)";
+        fprintf(textLbl, ...
+            nSamples, familyDistr, theta_concentration, theta_direction)
+
+    elseif ~po_cfg.sampling.theta  &&  po_cfg.sampling.r  % only r
+        textLbl = "you want to sample %u values of r (for uniform theta) from a %s distribution with parameters %f (concentration) and %f (direction)";
+        fprintf(textLbl, ...
+            nSamples, familyDistr, r_concentration, r_direction)
+
+    elseif po_cfg.sampling.theta  &&  po_cfg.sampling.r  % both theta and r
+        textLbl = "you want to sample %u values of r from a %s distribution with parameters %f (concentration) and %f (direction) and of theta from a %s distribution with parameters %f (concentration) and %f (direction)";
+        fprintf(textLbl, ...
+            nSamples, familyDistr, r_concentration, r_direction, familyDistr, theta_concentration, theta_direction)
+
+    end
+    fprintf('\n')
+end
+
 %% implementation
+
+% UNTIL HERE OK
+rng(randomSeed)
+
+if      po_cfg.sampling.theta  &&  ~po_cfg.sampling.r   % simulate theta
+
+    % drawing theta samples (step 3) from a probability mass
+    % distribution obtained from an empirical dendisty distribution
+    % (step 2), which is obtained from a theoretical distribution (sum
+    % of von Mises).
+
+    % step 1. create an angular grid in (-pi, pi]
+    thetaCoverage = 10000; % hard-coded but not important. it needs to provide a good coverage of the angular space
+    thetaLim = [-pi, pi];  % currently: [-pi, pi]
+    theta = linspace(thetaLim(1), thetaLim(2), thetaCoverage+1);
+    theta(1) = [];         % now: (-pi, pi] 
+
+    % step 2. empirical density distribution
+    kappa = theta_concentration;
+    mu    = theta_direction;
+    vmDensity = po_vonMisesDensity(kappa,mu,theta);
+
+    % step 3. probability mass distribution
+    vmMass = vmDensity * (theta(2) - theta(1)); % approximate area per bin
+    vmMass = vmMass / sum(vmMass);              % make sure it sums to 1
+
+    % step 4. draw theta samples from probability mass distribution
+    sampleIdx = randsample(length(theta), nSamples, true, vmMass);
+    samples = theta(sampleIdx);
+
+    % ensure vectors are column
+    theta = samples(:);
+    r = ones(size(theta));
+
+
+elseif ~po_cfg.sampling.theta  &&   po_cfg.sampling.r   % simulate r, for uniform theta
+elseif  po_cfg.sampling.theta  &&   po_cfg.sampling.r   % simulate r, for simulated theta
+end
+
+%% older section
 switch num2str(theta_rho)
     case num2str([0 1])  % simulate rho with uniform or given theta
         % build an empirical density distribution by drawing from a
@@ -99,10 +309,10 @@ switch num2str(theta_rho)
         theta(1) = [];
         r = po_vonMisesDensity(kappa,mu,theta);
         % step 2. probability mass distribution
-        pmf = r * (theta(2) - theta(1)); % approximate area per bin
-        pmf = pmf / sum(pmf);              % make sure it sums to 1
+        vmMass = r * (theta(2) - theta(1)); % approximate area per bin
+        vmMass = vmMass / sum(vmMass);              % make sure it sums to 1
         % step 3. draw theta samples from probability mass distribution
-        sampleIdx = randsample(length(theta), nSamples, true, pmf);
+        sampleIdx = randsample(length(theta), nSamples, true, vmMass);
         samples = theta(sampleIdx);
 
         % ensure vectors are column
