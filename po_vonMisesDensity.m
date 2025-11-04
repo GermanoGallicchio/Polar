eb992fe420f1176bceb208a9eb297a5afeb0c00f
Germano Gallicchio <germano.gallicchio@gmail.com>
2025-11-04 17:05:35 +0000

    Update po_vonMisesDensity.m

--- po_vonMisesDensity.m
+++ po_vonMisesDensity.m
@@ -1,4 +1,4 @@
-function [rho] = po_vonMisesDensity(kappa,mu,theta)
+function [vMdensity] = po_vonMisesDensity(kappa,mu,thetaGrid)
 
 % DESCRIPTION:
 %
@@ -11,30 +11,33 @@ function [rho] = po_vonMisesDensity(kappa,mu,theta)
 %   Germano Gallicchio (germano.gallicchio@gmail.com)
 
 %% shortcuts
-nModes = length(mu);
-nSamples = length(theta);
+
+nModes      = length(mu);
+nGridPoints = length(thetaGrid);
 
 %% implementation
-% von Mises density distribution (Berens book p.16, Pewsey book p.56)
-% mean of (potentially multiple) von Mises distribution(s)
 
-vMdensity_perMode = nan(nModes,nSamples);
+% compute the von Mises density distribution using Bessel function (Berens book p.16, Pewsey book p.56)
+vMdensity_perMode = nan(nModes,nGridPoints); % initialize
 for mIdx = 1:nModes
-    
-    vMdensity_perMode(mIdx,:) = (1/(2*pi*besseli(0,kappa(mIdx)))) * exp(kappa(mIdx) * cos(theta - mu(mIdx)));
+    vMdensity_perMode(mIdx,:) = (1/(2*pi*besseli(0,kappa(mIdx)))) * exp(kappa(mIdx) * cos(thetaGrid - mu(mIdx)));
 end
+
+% Average with equal weighting across modes to create a mixture of 
+% vonMises distributions. No mixtures if only one mode is provided.
 vMdensity = mean(vMdensity_perMode,1);
 
 %% sanity checks
 % sanity check: valid density distribution
+
 % non negative everywhere
 if any(vMdensity<0)
     error('likely coding bug: there aer some negative values in the density distribution')
 end
 % its integral is 1
-distrIntegral = trapz(theta,vMdensity);
-if abs(distrIntegral-1) > 1e-4
-    %             warning([' the integral of the function is ' num2str(distrIntegral) ', just short of 1. Increase the number of observations (currently: '  num2str(nSamples) ') to improve the distribution'])
+distrIntegral = trapz(thetaGrid,vMdensity);
+if abs(distrIntegral-1) > 1e-3
+    warning([' the integral of the function is ' num2str(distrIntegral) ', just short of 1. Increase the number of points in the grid (currently: '  num2str(nGridPoints) ') to improve the distribution'])
 end
 
 %% add noise
@@ -49,17 +52,5 @@ end
         % ---
         
 %%
-rho = vMdensity(:);
-
-
-
-
-
-
-
-
-
-
-
-
+vMdensity = vMdensity(:);
 
